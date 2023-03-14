c62e1b6e4d0ddcaaa1149ecf5efd52c0e4c8d13a
Oribe Sofia <sofia.oribe@hotmail.com>
2023-03-14 19:53:17 -0300

    ejercicio 2

---
+++ segundo-trabajo/datos.js
@@ -0,0 +1,12 @@
+const nacimiento = new Date("Diciembre 12 2001");
+
+const libro = {
+    titulo: "Padre rico, padre pobre",
+    autor: "Robert Kiyosaki",
+    fecha: 1997,
+    url: "https://www.emprenderalia.com/resena-de-rich-dad-poor-dad-de-robert-kiyosaki/"
+}
+
+const listaPersonal = ["Sofia Oribe", 21, true, nacimiento, libro];
+
+console.log(listaPersonal);
