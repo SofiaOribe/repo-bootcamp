459f20646f468348f74b6e00b156cb540bda7526
Oribe Sofia <sofia.oribe@hotmail.com>
2023-03-14 19:01:28 -0300

    creo repo

---
+++ index.js
@@ -0,0 +1,8 @@
+//Este es el mensaje de bienvenida 
+console.log("Esta es la puerta de entrada al proyecto");
+
+//Esta es la parte intermedia del proyecto
+console.log("Chequeo");
+
+//Mensaje de despedida
+console.log("Adiós");
