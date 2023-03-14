bd1648ca82629939def48cd0155a4e6adec2bad6
Oribe Sofia <sofia.oribe@hotmail.com>
2023-03-14 19:04:18 -0300

    Proyecto node que contiene un script para saludar

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
 //Este es el mensaje de bienvenida 
-console.log("Esta es la puerta de entrada al proyecto");
-
-//Esta es la parte intermedia del proyecto
-console.log("Chequeo");
-
-//Mensaje de despedida
-console.log("Adiós");
+console.log("Este es el inicio de mi proyecto");
