25a8237e9fa341a31bb8e7037babc867787a2817
Oribe Sofia <sofia.oribe@hotmail.com>
2023-03-18 12:13:02 -0300

    pequeñas correciones

--- cuarto-trabajo/string.js
+++ cuarto-trabajo/string.js
@@ -6,13 +6,12 @@ let estudiante = nombre.concat(" ", apellido);
 let estudianteMayus = estudiante.toUpperCase();
 let estudianteMinus = estudiante.toLowerCase();
 
-let cantLetras = estudiante.trim().length;
+let cantLetras = estudiante.length;
 
 let primeraLetraN = nombre.charAt(0);
 
-let ultimaLetraA = apellido.charAt(4);
+let ultimaLetraA = apellido[apellido.length - 1];
 
 let eliminarEspacio = estudiante.replace(/ /g, '');
 
-let existe = estudiante.includes("Sofia");
-
+let existe = estudiante.includes(nombre);
