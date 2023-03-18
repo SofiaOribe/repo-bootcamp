2972b5a2205b83bd167aa45c0351cc7c54f424ed
Oribe Sofia <sofia.oribe@hotmail.com>
2023-03-18 12:04:50 -0300

    cuarto trabajo

---
+++ cuarto-trabajo/string.js
@@ -0,0 +1,18 @@
+let nombre = "Sofia";
+let apellido = "Oribe";
+
+let estudiante = nombre.concat(" ", apellido);
+
+let estudianteMayus = estudiante.toUpperCase();
+let estudianteMinus = estudiante.toLowerCase();
+
+let cantLetras = estudiante.trim().length;
+
+let primeraLetraN = nombre.charAt(0);
+
+let ultimaLetraA = apellido.charAt(4);
+
+let eliminarEspacio = estudiante.replace(/ /g, '');
+
+let existe = estudiante.includes("Sofia");
+
