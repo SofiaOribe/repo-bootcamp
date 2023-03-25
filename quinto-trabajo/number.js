780fe1328a20217b0b1e7f8041ff4201d582c206
Oribe Sofia <sofia.oribe@hotmail.com>
2023-03-24 23:17:35 -0300

    trabajo cinco

---
+++ quinto-trabajo/number.js
@@ -0,0 +1,7 @@
+let altura = 159;
+let alturaFloat = 1.59;
+let pesoFloat = 41.0;
+let alturaArriba = alturaFloat.toFixed(1);
+let pesoAbajo = pesoFloat.toFixed(0)
+let max_value =  ((Number.MAX_VALUE + 1) === Number.MAX_VALUE);
+
